b0db174a757790c46faeaa6eee96707cdadf9952
Tchasinga jacques <tchasingajacques@gmail.com>
2023-10-30 17:58:36 +0200

    new

--- src/Components/Mycomponents.jsx
+++ src/Components/Mycomponents.jsx
@@ -10,7 +10,6 @@ function MyComponent() {
         <meta property="og:url" content="https://stuff-seller.netlify.app/" />
         <meta property="og:type" content="website" />
       </Helmet>
-      <h1>Hello there</h1>
     </div>
   );
 }
