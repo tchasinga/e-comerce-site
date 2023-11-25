1b2187189e26324d7e2b25f98b047e77bf8f0060
Tchasinga jacques <tchasingajacques@gmail.com>
2023-11-25 11:42:03 +0200

    new gold updating

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import { FiUser, FiHeart, FiShoppingCart } from 'react-icons/fi';
 
 const Header = () => {
     return (
-        <div className="fixed top-0 left-0  right-0 bg-black z-10">
+        <div className="fixed top-0 left-0  right-0 bg-black z-10 zone">
             <div className="header flex mt-4 mb-4 mx-0 px-40 flex-wrap items-center justify-between">
                 <div className="firstnav">
                     <ul className="flex gap-3 items-center hover:cursor-pointer">
