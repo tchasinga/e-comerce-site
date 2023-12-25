0fe09630439d51b18adc872bdafd308290f88e37
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-25 16:02:37 +0200

    mySwiper

--- frontend/src/Components/Getproduct.jsx
+++ frontend/src/Components/Getproduct.jsx
@@ -1,7 +1,23 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import mygoals from '../Projects/Objectseven';
+import { Pagination } from 'swiper/modules';
+import 'tailwindcss/tailwind.css'; // Import the Tailwind CSS
+
 const Getproduct = () => {
   return (
-    <div className=" mt-7 mb-7 designcode gap-4 flex">
-      
+    <div className='container mx-auto my-10'>
+      <Swiper pagination={true} modules={[Pagination]} className="mySwiper bg-gray-100 rounded-md overflow-hidden">
+        {mygoals.map((mygoal) => (
+          <SwiperSlide key={mygoal.id} className='relative'>
+            <img src={mygoal.img} alt={mygoal.title} className='w-full flex justify-center items-center h-64 object-cover' />
+            <div className='absolute inset-0 flex items-center justify-center text-white font-bold text-xl bg-black bg-opacity-50'>
+              {mygoal.title}
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
