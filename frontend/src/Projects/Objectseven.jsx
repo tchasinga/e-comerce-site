70a758d4aa2991c4e09a84901554a714bf065b7e
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-25 15:23:26 +0200

    new code is added

---
+++ frontend/src/Projects/Objectseven.jsx
@@ -0,0 +1,29 @@
+import OneImg from '../Img/HonorMagicBook.png';
+import TwoImg from '../Img/pngegg.png';
+import ThreeImg from '../Img/AmbraSkin.png';
+import FourImg from '../Img/productthumbnail.png';
+
+const mygoals = [
+    {
+        id: 1,
+        img: OneImg,
+        title: 'Honor MagicBook',
+    },
+    {
+        id: 2,
+        img: TwoImg,
+        title: 'toy magic book',
+    },
+    {
+        id: 3,
+        img: ThreeImg,
+        title: 'Honor MagicBook',
+    },
+    {
+        id: 4,
+        img: FourImg,
+        title: 'Honor MagicBook',
+    },
+]
+
+export default mygoals;
