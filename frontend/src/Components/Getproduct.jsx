0140a2889248d85c3e975ba663932dcb70a5404c
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-25 17:04:36 +0200

    new updating is added

--- frontend/src/Components/Getproduct.jsx
+++ frontend/src/Components/Getproduct.jsx
@@ -1,42 +1,30 @@
+import mygoals from "../Projects/Objectseven";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+// import required modules
+import { Pagination } from "swiper/modules";
+
 const Getproduct = () => {
   return (
-    <div className=" mt-7 mb-7 designcode gap-4 flex">
-      <div className="flexContainer bg-gray-100">
-        <div className="ImgGetairpod"></div>
-        <div>
-          <div className="relative top-40">
-            <p>Beauty picks</p>
-          </div>
-        </div>
-      </div>
-
-
-      <div className="flexContainer bg-gray-100">
-        <div className="ImgGetairpod"></div>
-        <div>
-          <div className="relative top-40">
-            <p>Computer & Accessories</p>
-          </div>
-        </div>
-      </div>
-
-      <div className="flexContainer bg-gray-100">
-        <div className="ImgGetairpod"></div>
-        <div>
-          <div className="relative top-40">
-            <p>Video games</p>
-          </div>
-        </div>
-      </div>
-
-      <div className="flexContainer bg-gray-100">
-        <div className="ImgGetairpod"></div>
-        <div>
-          <div className="relative top-40">
-            <p>Toys & Games</p>
-          </div>
-        </div>
-      </div>
+    <div className="h-full w-full mt-7 mb-7 designcode  flex justify-center">
+      <Swiper pagination={{dynamicBullets: true,}} modules={[Pagination]} className="mySwiper h-full w-full">
+        {mygoals.map((val) => {
+          return (
+            <SwiperSlide key={val.id}>
+              <div className="flex justify-center items-center">
+                <img
+                  className="object-contain h-96 w-full"
+                  src={val.imgsrc}
+                  alt={val.title}
+                />
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </div>
   );
 };
